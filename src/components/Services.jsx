b9dd812ec7e3370373e4cad80e20a96a80e24f67
DenisProsolovich <aprildeadscream@gmail.com>
2023-04-15 11:34:55 +0300

    web

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -31,43 +31,39 @@ function Services() {
           </p>
           <div className="services-cards">
             <div className="services-card">
-              <Image src={Card} alt="card" className="card-item" />
-              <Image src={CardMobile} alt="card" className="card-item-mobile" />
+              <Image src={Card} alt="service-card" className="card-item" />
+              <Image src={CardMobile} alt="service-card" className="card-item-mobile" />
               <div className="card-text">
                 <p className="card-sub-title">For Investors & Wholesalers</p>
-                <p className="card-title">Investor&apos;s Gateway to Lucrative Opportunities</p>
+                <h5 className="card-title">Investor&apos;s Gateway to Lucrative Opportunities</h5>
               </div>
               <div>
                 <Link href="https://landsupplier.com/partners" className="card-link">
-                  <button type="button" className="card-button">
-                    Explore More
-                  </button>
+                  Explore More
                   <span className="card-arrow" />
                 </Link>
               </div>
             </div>
             <div className="services-card">
-              <Image src={Card2} alt="card" className="card-item" />
-              <Image src={Card2Mobile} alt="card" className="card-item-mobile" />
+              <Image src={Card2} alt="service-card" className="card-item" />
+              <Image src={Card2Mobile} alt="service-card" className="card-item-mobile" />
               <div className="card-text">
                 <p className="card-sub-title">A-to-Z Business Solution</p>
-                <p className="card-title">
+                <h5 className="card-title">
                   Launch Your Successful Real Estate Venture
                   <br /> for Under $300
-                </p>
+                </h5>
               </div>
               <div className="card-link">
-                <button type="button" className="card-button">
-                  Explore More
-                </button>
+                Explore More
                 <span className="card-arrow" />
               </div>
             </div>
             <div className="services-card">
-              <Image src={Card3} alt="card" className="card-item" />
-              <Image src={Card3Mobile} alt="card" className="card-item-mobile" />
+              <Image src={Card3} alt="service-card" className="card-item" />
+              <Image src={Card3Mobile} alt="service-card" className="card-item-mobile" />
               <div className="card-text">
-                <p className="card-title">Real Estate Acquisitions Data</p>
+                <h5 className="card-title">Real Estate Acquisitions Data</h5>
                 <p className="services-sub-title_any">
                   Acquire Extensive Real Estate Data, Including Homes, Land, Mobile Homes, Condos,
                   And Multi-Family Units. Access Mailing Addresses, Emails, And Phone Numbers For
@@ -75,71 +71,63 @@ function Services() {
                 </p>
               </div>
               <div className="card-link">
-                <button type="button" className="card-button">
-                  Contact Us
-                </button>
+                Contact Us
                 <span className="card-arrow" />
               </div>
             </div>
             <div className="services-card">
-              <Image src={Card4} alt="card" className="card-item" />
-              <Image src={Card4Mobile} alt="card" className="card-item-mobile" />
+              <Image src={Card4} alt="service-card" className="card-item" />
+              <Image src={Card4Mobile} alt="service-card" className="card-item-mobile" />
               <div className="card-text">
                 <p className="card-sub-title">Seeking a Fast Property Sale?</p>
-                <p className="card-title">
+                <h5 className="card-title">
                   Effortlessly Sell Your Property and Receive a Direct, Fast Cash Offer with Our
                   Streamlined Process
-                </p>
+                </h5>
               </div>
               <div>
                 <Link href="https://www.landville.com/sell-land" className="card-link">
-                  <button type="button" className="card-button">
-                    Explore More
-                  </button>
+                  Explore More
                   <span className="card-arrow" />
                 </Link>
               </div>
             </div>
             <div className="services-card">
-              <Image src={Card5} alt="card" className="card-item" />
-              <Image src={Card5Mobile} alt="card" className="card-item-mobile" />
+              <Image src={Card5} alt="service-card" className="card-item" />
+              <Image src={Card5Mobile} alt="service-card" className="card-item-mobile" />
               <div className="card-text">
                 <p className="card-sub-title">Get a Fantastic Deal</p>
-                <p className="card-title">Search LandSupplier’s Wholesale Property Deals</p>
+                <h5 className="card-title">Search LandSupplier’s Wholesale Property Deals</h5>
               </div>
               <div>
                 <Link href="https://landsupplier.com" className="card-link">
-                  <button type="button" className="card-button">
-                    Explore More
-                  </button>
+                  Explore More
                   <span className="card-arrow" />
                 </Link>
               </div>
             </div>
             <div className="services-card">
-              <Image src={Card6} alt="card" className="card-item" />
-              <Image src={Card6Mobile} alt="card" className="card-item-mobile" />
+              <Image src={Card6} alt="service-card" className="card-item" />
+              <Image src={Card6Mobile} alt="service-card" className="card-item-mobile" />
               <div className="card-text">
                 <p className="card-sub-title">Properties Marketplace</p>
-                <p className="card-title">The Ultimate Platform Connecting Buyers and Sellers</p>
+                <h5 className="card-title">The Ultimate Platform Connecting Buyers and Sellers</h5>
               </div>
               <div>
                 <Link href="https://landville.com/" className="card-link">
-                  <button type="button" className="card-button">
-                    Explore More
-                  </button>
+                  Explore More
                   <span className="card-arrow" />
                 </Link>
               </div>
             </div>
             <div className="services-card">
-              <Image src={Card7} alt="card" className="card-item" />
-              <Image src={Card7Mobile} alt="card" className="card-item-mobile" />
+              <Image src={Card7} alt="service-card" className="card-item" />
+              <Image src={Card7Mobile} alt="service-card" className="card-item-mobile" />
               <div className="card-text">
                 <p className="card-sub-title gray-text-color">Sell for Free</p>
-                <p className="card-title gray-text-color">
+                <h5 className="card-title gray-text-color">
                   Begin Marketing Your Property at No Cost and Experience a Seamless Selling Journey
-                </p>
+                </h5>
               </div>
               <div className="card-link">
                 <button type="button" className="card-button gray-text-color ">
@@ -148,13 +136,13 @@ function Services() {
               </div>
             </div>
             <div className="services-card">
-              <Image src={Card8} alt="card" className="card-item" />
-              <Image src={Card8Mobile} alt="card" className="card-item-mobile" />
+              <Image src={Card8} alt="service-card" className="card-item" />
+              <Image src={Card8Mobile} alt="service-card" className="card-item-mobile" />
               <div className="card-text">
                 <p className="card-sub-title gray-text-color">Focused Audience Outreach</p>
-                <p className="card-title gray-text-color">
+                <h5 className="card-title gray-text-color">
                   Acquire Cost-Effective, Targeted Leads at Unbeatable Prices in The Market
-                </p>
+                </h5>
               </div>
               <div className="card-link">
                 <button type="button" className="card-button gray-text-color">
